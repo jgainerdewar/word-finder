9f38c417ee3967e98019433fc694a705a2195b3d
Janet Gainer-Dewar <janet.dewar@gmail.com>
2020-03-05 13:25:21 -0500

    Some initial project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='WordFinder',
+      version='0.1',
+      description='CLI tool for finding potential words in a 2D array of letters',
+      author='Janet Gainer-Dewar',
+      author_email='janet.dewar@gmail.com',
+      url='https://github.com/jgainerdewar/word-finder',
+      packages=['wordfinder'],
+     )

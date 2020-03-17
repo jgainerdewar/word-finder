b3c5aa06141340be26a73ab6b76178e08e15c9e3
Janet Gainer-Dewar <janet.dewar@gmail.com>
2020-03-17 14:43:00 -0400

    Update CLI to provide full puzzle solutions

--- wordfinder/__main__.py
+++ wordfinder/__main__.py
@@ -1,6 +1,7 @@
 import csv
 import sys
 from argparse import ArgumentParser
+import wordfinder
 from wordfinder import LetterMatrix, Dictionary
 
 def construct_matrix(input_csv):
@@ -20,22 +21,26 @@ def main(args=None):
     args = sys.argv[1:]
 
   parser = ArgumentParser("wordfinder", 
-                          description="Find a word of a specified length " +
-                          "in a 2D array of letters")
+                          description="Find words of the specified lengths " +
+                          "in a 2D array of letters. As each word is found, " +
+                          "its letters are removed from the array and the " +
+                          "remaining letters drop down within each column.")
   parser.add_argument("input_csv", type=str,
                       help="Path to a CSV file containing letter matrix. " +
                       "Each element in CSV must either be empty, or contain " +
                       "a single letter A-Z or a-z. All rows must be the " +
                       "same length.")
-  parser.add_argument("word_length", type=int,
-                      help="The length of the word to find.")
+  parser.add_argument("word_lengths", type=int, nargs="+",
+                      help="The lengths of the word to find.")
   parser.add_argument("--prefix", "-p", type=str, default="", 
                       help="If provided, only words with this prefix " +
-                      "will be returned. Can be any length up to word length.")
+                      "will be returned. Can be any length up to word length." +
+                      "Only valid when searching for a single word. ")
   parser.add_argument("--raw_results", dest="use_dict", action="store_false",
                       help="By default we return only words that appear in " +
                       "the configured dictionary. Use this option to return " +
-                      "every possible solution instead.")
+                      "every possible solution instead. Note that this may " +
+                      "return a truly huge number of results.")
   parser.add_argument("--dictionary_path", type=str, default="",
                      help="Use this option to define a custom dictionary to " +
                      "pull solutions from. If not set, we attempt to use " +
@@ -44,21 +49,39 @@ def main(args=None):
 
   parsed_args = parser.parse_args(args)
 
+  if len(parsed_args.word_lengths) > 1 and parsed_args.prefix != "":
+    print("Prefix is only valid when looking for a single word.")
+    sys.exit(1)
+
   dictionary = None
   if parsed_args.use_dict:
     dictionary = Dictionary(parsed_args.dictionary_path)
 
   matrix = construct_matrix(parsed_args.input_csv)
-  words = matrix.find_words(
-    parsed_args.word_length, 
-    parsed_args.prefix, 
-    dictionary
-  )
 
-  if not words:
-    print("No results")
-  for word in words:
-    print(word.upper())
+  if parsed_args.prefix:
+    solutions = [
+      matrix.find_words(
+        parsed_args.word_lengths[0], 
+        parsed_args.prefix, 
+        dictionary
+      )
+    ]
+  else:
+    solutions = wordfinder.solve_word_game(
+      matrix,
+      parsed_args.word_lengths, 
+      dictionary
+    )
+
+  sep = "-"*20
+  if not solutions:
+    print("No solutions found")
+  for solution in sorted(set(solutions)):
+    print(sep)
+    for word in solution:
+      print(word.upper())
+  print(sep)
 
     
 if __name__ == "__main__":
